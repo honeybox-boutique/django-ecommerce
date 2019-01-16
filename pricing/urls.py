f745c0cd7b459df036c9d95ab25c8d74e298818f
eezzo <derastafari@gmail.com>
2019-01-16 10:55:39 -0700

    Created purchasing app and set up urls.py and added it to settings.py as installed app.

--- pricing/urls.py
+++ pricing/urls.py
@@ -13,17 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+app_name = 'purchasing'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('products/', include('products.urls')),
-    path('purchases/', include('purchases.urls')),
-    path('pricing/', include('pricing.urls')),
-    path('', TemplateView.as_view(template_name="index.html"), name='home'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
