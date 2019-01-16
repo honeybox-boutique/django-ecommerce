93f60a8be064387f07d5af2480d37359f09ccff9
eezzo <derastafari@gmail.com>
2019-01-16 11:32:06 -0700

    Added pricing table to data model for pricing app and made migrations.

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Pricing
 # Register your models here.
+
+class PricingAdmin(admin.ModelAdmin):
+    model = Pricing
+admin.site.register(Pricing, PricingAdmin)
--- pricing/models.py
+++ pricing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+
+class Pricing(models.Model):
+    pricingID = models.AutoField(primary_key=True)
+    pricingBasePrice = models.DecimalField(max_digits=8, decimal_places=3)
+    pricingDateCreated = models.DateTimeField('date created')
+    pricingStartDate = models.DateTimeField('start date')
+    pricingEndDate = models.DateTimeField('end date')
+    pricingNote = models.TextField(max_length=200)
+
+    productID = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'pricing'
+
+    def __str__(self):
+        return self.pricingNote
