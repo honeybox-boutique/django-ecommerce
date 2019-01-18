6497dfbc8ba6e5a7c0f5eff042c7a2706d25c122
eezzo <derastafari@gmail.com>
2019-01-18 14:46:10 -0700

    Added PDiscount and CDiscount classes to model and made migrations.

--- pricing/admin.py
+++ pricing/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Pricing
+from .models import Pricing, PDiscount
 # Register your models here.
 
 class PricingAdmin(admin.ModelAdmin):
     model = Pricing
 admin.site.register(Pricing, PricingAdmin)
+
+class PDiscountAdmin(admin.ModelAdmin):
+    model = PDiscount
+admin.site.register(PDiscount, PDiscountAdmin)
