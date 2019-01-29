ddada119df12e3a9fd76df0d6c9d0bd41aeed8e7
eezzo <derastafari@gmail.com>
2019-01-28 17:07:14 -0700

    Added add to cart form with post method and verified all params were being received by server.

--- shopcart/views.py
+++ shopcart/views.py
@@ -10,13 +10,19 @@ from .models import ShopCart, PurchaseItems
 
 def cart_home(request):
     cart_obj, new_obj = ShopCart.objects.get_or_new(request)
-    return render(request, 'carts/home.html', {})
+    return render(request, 'carts/home.html', {"cart": cart_obj})
 
 def cart_update(request):
     """ should use product-slug, color, and size as parameters to check if:
         purchitem with matching slug, color, and size exists,
         AND does NOT have a sale transaction associated with it
     """
+    print(request.POST)
+    # get POST parameters
+    product_slug = request.POST.get('productSlug')
+    color = request.POST.get('color-selection')
+    size = request.POST.get('size-selection')
+    quantity = request.POST.get('quantity')
     product_id = 7
     purchase_item_obj = PurchaseItems.objects.get(prodStockID=product_id)
     cart_obj, new_obj = ShopCart.objects.get_or_new(request)
