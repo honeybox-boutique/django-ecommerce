3a3270891c45efe04c63ec1a47db0db4c2de3f70
eezzo <derastafari@gmail.com>
2019-01-29 14:27:18 -0700

    Added billing data model. Adjusted user signup to take all info.

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,32 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+class BillingProfile(models.Model):
+    billingEmail = models.EmailField()
+    active = models.BooleanField(default=True)
+    lastUpdated = models.DateTimeField(auto_now=True)
+    dateCreated = models.DateTimeField(auto_now_add=True)
+
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    # billingToken = ? #Stripe token here, will be retrieved using sensors
+
+    def __str__(self):
+        return self.billingEmail
+
+# Stripe tokenizer API request
+# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
+    # if created:
+        # print('Actual api request to Stripe to get token')
+        # instance.customer_id = newID
+        # instance.save()
+
+# creates billing profile on user creation
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(user=instance, billingEmail=instance.email)
+post_save.connect(user_created_receiver, sender=User)
--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import FormView, CreateView
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 # Create your views here.
 
 def dashboard(request):
     return render(request, 'users/dashboard.html')
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'users/signup.html'
     success_url='/users/login/?next=/'
