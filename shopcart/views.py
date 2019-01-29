8d5d5cb1e597b6f0038be9263de653d368953c8d
eezzo <derastafari@gmail.com>
2019-01-28 19:03:30 -0700

    Added add to cart functionality for product detail pages. Quantity field currently does nothing. If not enough stock available redirect does not display error message.

--- shopcart/views.py
+++ shopcart/views.py
@@ -10,7 +10,7 @@ from .models import ShopCart, PurchaseItems
 
 def cart_home(request):
     cart_obj, new_obj = ShopCart.objects.get_or_new(request)
-    return render(request, 'carts/home.html', {"cart": cart_obj})
+    return render(request, 'carts/home.html', {"shopcart": cart_obj})
 
 def cart_update(request):
     """ should use product-slug, color, and size as parameters to check if:
@@ -24,7 +24,16 @@ def cart_update(request):
     size = request.POST.get('size-selection')
     quantity = request.POST.get('quantity')
     product_id = 7
-    purchase_item_obj = PurchaseItems.objects.get(prodStockID=product_id)
-    cart_obj, new_obj = ShopCart.objects.get_or_new(request)
+    cart_obj, new_obj = ShopCart.objects.get_or_new(request)# get cart
+            # ^exclude pitems already in cart_obj
+    # purchase_item_obj = PurchaseItems.objects.get(prodStockID=product_id)
+    purchase_item_query = PurchaseItems.objects.filter(productID__productSlug=product_slug, piColor__colorName=color).filter(piSize=size)
+    for cart_item in cart_obj.shopCartItems.all():# exclude all pitems already in cart
+        purchase_item_query = purchase_item_query.exclude(pk=cart_item.pk)
+    # if quantity + quantity in cart > purchase_item_query.count() return "not enough in stock to add these to cart"
+    if not purchase_item_query:
+        print('Not Enough')
+        return redirect('shopcart:home')
+    purchase_item_obj = purchase_item_query.first()
     cart_obj.shopCartItems.add(purchase_item_obj)
     return redirect('shopcart:home')
