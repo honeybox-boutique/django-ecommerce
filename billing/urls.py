5a128f7b5d8738c43222a04d081f2274acb23623
eezzo <derastafari@gmail.com>
2019-02-08 07:37:43 -0700

    Added initial stripe implementation. Ajax is not set up correctly with CSRF tokens so this will not work until static files are fixed.

--- billing/urls.py
+++ billing/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 app_name = 'billing'
 urlpatterns = [
-] 
+    path('payment-method/', views.payment_method_view, name='payment_method'),
+    path('payment-method/create/', views.payment_method_create_view, name='payment_method_create'),
+]
--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,20 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
+import stripe
+stripe.api_key = "***REMOVED***"
+STRIPE_PUB_KEY = '***REMOVED***'
+
 # Create your views here.
+def payment_method_view(request):
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', {'publish_key': STRIPE_PUB_KEY})
+
+
+
+def payment_method_create_view(request):
+    if request.method == 'POST' and request.is_ajax():
+        print(request.POST)
+        return JsonResponse({'message': 'Done'})
+    return HttpResponse('error', status_code=401)
