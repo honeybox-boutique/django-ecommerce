c67fffdeeed12013f429f2055e4d7869a0b7c480
eezzo <derastafari@gmail.com>
2019-02-20 20:14:12 -0700

    Added debug toolbar to help debugging.

--- speedtest/urls.py
+++ speedtest/urls.py
@@ -39,3 +39,13 @@ urlpatterns = [
     path('contact-us/thanks/', email_success_view, name='contact_thanks'),
     path('coming-soon/', TemplateView.as_view(template_name="coming-soon.html"), name='coming-soon'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
