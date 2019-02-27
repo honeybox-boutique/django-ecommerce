2b22336e73a5be4e7532d9e548b93a19581e3de6
eezzo <derastafari@gmail.com>
2019-02-27 05:40:33 -0700

    Fixed login and signup form formatting and stuff.

--- users/forms.py
+++ users/forms.py
@@ -1,20 +1,40 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-# username
-# password
-# email
-# first_name
-# last_name
+
+
+
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
-
     class Meta:
+        fields = ("email", "password1", "password2", "first_name", "last_name")
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', )
+        labels = {
+            'email': '',
+            'password1': '',
+            'password2': '',
+            'first_name': '',
+            'last_name': '',
+        }
+        help_texts = {
+            'email': '',
+            'password1': '',
+            'password2': '',
+            'first_name': '',
+            'last_name': '',
+        }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs.update({'class': 'form-control', 'placeholder': 'Email*'})
+        self.fields["first_name"].widget.attrs.update({'class': 'form-control', 'placeholder': 'First Name'})
+        self.fields["last_name"].widget.attrs.update({'class': 'form-control', 'placeholder': 'Last Name'})
+        self.fields["password1"].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password*'})
+        self.fields["password2"].widget.attrs.update({'class': 'form-control', 'placeholder': 'Reenter Password*'})
+        self.fields["email"].help_text = None
+        self.fields["first_name"].help_text = None
+        self.fields["last_name"].help_text = None
+        # self.fields["password1"].help_text = None
+        self.fields["password2"].help_text = None
 class UserAccountInfoForm(forms.ModelForm):
     
     username = forms.CharField(disabled=True, label='Username')
